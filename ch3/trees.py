d1a661070a2cece2e0077dd3648226c6d8dbe4ed
zhql0907 <zhql0907@outlook.com>
2016-09-22 19:53:22 +0800

    add function to choose best feature to split

--- ch3/trees.py
+++ ch3/trees.py
@@ -24,11 +24,37 @@ def createDataSet():
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
 
+# This function return the data that have the 'value' in 'axis'
 # dataSet is the dataSet to be divided
-# axis is the feature used to divide the dataSet
+# axis is the feature used to divide the dataSet(here it is a number, representing the index of the feature
 # value is the dataSet to reutrn
+
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
-        if
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
 
+# the entropy of a dataSet is different from
+# sum of the small dataSet divided from the dataSet
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy =calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
